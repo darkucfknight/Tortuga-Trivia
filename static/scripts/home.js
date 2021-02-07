0a602f2e6a34fc3535df1de8b9919763b5fab99a
darkucfknight <jordan.layton95@gmail.com>
2021-02-06 23:55:59 -0500

    Add auth for only admin creation

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -87,6 +87,43 @@ const genSubsRef = firebase.database().ref().child('GeneralSubcategories');
 
 var currSortedRef = geekSortedRef;
 var currSubRef = geekSubsRef;
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function googleSignin() {
+    firebase
+        .auth()
+
+        .signInWithPopup(provider)
+        .then(function (result) {
+            var token = result.credential.accessToken;
+            var user = result.user;
+
+            console.log(token);
+            console.log(user);
+        })
+        .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(error.code);
+            console.log(error.message);
+        });
+}
+
+function googleSignout() {
+    firebase
+        .auth()
+        .signOut()
+
+        .then(
+            function () {
+                console.log('Signout Succesfull');
+            },
+            function (error) {
+                console.log('Signout Failed');
+            }
+        );
+}
 
 initBasePanel();
 
